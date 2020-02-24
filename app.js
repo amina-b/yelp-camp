03c92a4234c83c2470388ce523f12a836a321f97
Amina Balavac <abalavac1@etf.unsa.ba>
2020-02-24 20:14:13 +0100

    yelp sa registracijom

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
+var express     = require("express");
+var app         = express();
+var bodyParser  = require("body-parser");
+var mongoose    = require("mongoose");
+var Campgrounds = require("./models/campground");
+var seedDB      = require("./seeds");  
+var Comment     = require("./models/comment");
+var passport    = require("passport");
+var LocalStrategy = require("passport-local");
+var User          =require("./models/user");
+
+
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret:"secreeet!!",
+    resave: false,
+    saveUninitialized : false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("./public"));
+seedDB();
 
 mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true});
 
-var campSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-var Campgrounds = mongoose.model("campgrounds", campSchema);
 
 // PRIKAZIVANJE NASLOVNE STRANE
 
@@ -30,7 +45,7 @@ app.get("/campgrounds", function(req,res) {
         {
             console.log(err)
         } else {
-            res.render("campgrounds", { campgrounds: campgrounds});
+            res.render("campgrounds/index", { campgrounds: campgrounds, currentUser:req.user });
         }
     });  
 });
@@ -56,21 +71,104 @@ app.post("/campgrounds", function(req,res){
 // FORMA ZA DODAVANJE NOVOG CAMPGROUNDA
 
 app.get("/campgrounds/new",function(req,res){  
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 // PRIKAZIVANJE VISE INFORMACIJA O JEDNOM CAMPGROUNDU
 
 app.get("/campgrounds/:id", function(req,res){
-    Campgrounds.findById(req.params.id, function (err, campground){
+    Campgrounds.findById(req.params.id).populate("comments").exec(function (err, campground){
         if (err) {
             console.log(err)
         } else {
-            res.render("show", { campground: campground });
+            
+            res.render("campgrounds/show", { campground: campground });
         }
     });
 });
 
+
+// PRIKAZIVANJE FORME ZA KOMENTAR
+
+app.get("/campgrounds/:id/comments/new", function(req,res){
+    Campgrounds.findById(req.params.id, function (err, campground){
+        if (err) {
+            console.log(err)
+        } else {
+            res.render("comments/new", { campground: campground });
+        }
+    });
+});
+
+// KREIRANJE KOMENTARA
+
+app.post("/campgrounds/:id/comments",  function(req,res){
+    Campgrounds.findById(req.params.id, function (err, campground) {
+        if (err) {
+            console.log(err)
+        } else {
+            Comment.create(req.body.comment, function (err, comment) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            })
+        }
+    })
+});
+
+// REGISTRACIJA USERA
+
+app.get("/register", function(req,res){
+    res.render("register");
+});
+
+app.post("/register", function(req,res){
+    var newUser = new User ({ username : req.body.username});
+    User.register(newUser, req.body.password, function (err, user){
+        if (err) {
+            console.log(err);
+            return res.render("/register");
+        } else {
+                passport.authenticate("local")(req,res, function(){
+                res.redirect("/campgrounds");
+            })
+        }
+    });
+});
+
+//LOGIN USER
+
+app.get("/login", function(req,res){
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local",   
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req,res){
+});
+
+//LOGOUT USER
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+//MIDDLEWARE
+
+function isLoggedIn(req,res,next)
+{
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(8080, function(){
     console.log("Yeplcamp has started");
